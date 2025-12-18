c80bf6da066861e5127f51fce3dd189f0cdff6f2
Davi <davi.marques@dcx.ufpb.br>
2025-12-18 01:12:59 -0300

    Adicionando a tela Home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,16 @@
-import '../presentation/styles/App.css'
-import Home from '../presentation/pages/Home'
-import Questionario from '@/presentation/pages/Questionario'
+import "../presentation/styles/App.css";
+import Home from "../presentation/pages/Home";
+import Questionario from "@/presentation/pages/Questionario";
+import Principal from "@/presentation/pages/Principal";
 
 function App() {
-
   return (
     <>
-    <Home/>
-    <Questionario></Questionario>
+      <Home />
+      <Principal></Principal>
+      <Questionario></Questionario>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,15 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from '../presentation/pages/Home'
-import Questionario from '@/presentation/pages/Questionario'
-
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../presentation/pages/Home";
+import Questionario from "@/presentation/pages/Questionario";
+import Principal from "@/presentation/pages/Principal";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Principal />,
+  },
   {
     path: "/mapa",
     element: <Home />,
   },
   {
     path: "/perguntas",
-    element: <Questionario/>,
+    element: <Questionario />,
   },
 ]);
